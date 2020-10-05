59f85ea7968fd9b52690a222a80e424aa16f4489
wowen1 <70781327+wowen1@users.noreply.github.com>
2020-10-05 11:41:54 -0700

    AddingMachine.java; code skeleton

---
+++ AddingMachine.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.Scanner;
+
+package cse360assignment01
+
+public class AddingMachine()
+{
+   private int total;
+   private String history;
+   
+   public AddingMachine()
+   {
+      total = 0;
+      history = "";
+   }
+   
+   public getTotal()
+   {
+      return 0;
+   }
+   
+   public void add(int value)
+   {
+   
+   }
+   
+   public void subtract(int value)
+   {
+   
+   }
+   
+   public String toString()
+   {
+      return "";   
+   }      
+   
+   public void clear()
+   {
+   
+   }
+
+}   
+   
+      
+    
